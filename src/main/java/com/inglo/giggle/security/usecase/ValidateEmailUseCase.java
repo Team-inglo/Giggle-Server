317913ca68c5900dc0c4f8133dbef9594d5fff54
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-10-20 15:33:56 +0900

    [API-2.2] IGS-15 feat: 이메일 중복검사 구현

---
+++ src/main/java/com/inglo/giggle/security/usecase/ValidateEmailUseCase.java
@@ -0,0 +1,16 @@
+package com.inglo.giggle.security.usecase;
+
+import com.inglo.giggle.core.annotation.bean.UseCase;
+import com.inglo.giggle.security.dto.request.ValidateEmailRequestDto;
+import com.inglo.giggle.security.dto.response.ValidationResponseDto;
+
+@UseCase
+public interface ValidateEmailUseCase {
+
+    /**
+     * 이메일 인증 코드 발급
+     * @param email 이메일 인증 요청 DTO
+     * @return 이메일 인증 응답 DTO
+     */
+    ValidationResponseDto execute(String email);
+}
