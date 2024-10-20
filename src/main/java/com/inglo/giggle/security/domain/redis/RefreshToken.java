c67ba2ab75478c7f7c2c23d05491efbf2745acf3
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-10-20 15:18:52 +0900

    IGS-15 feat: RefreshToken 도메인 구현

---
+++ src/main/java/com/inglo/giggle/security/domain/redis/RefreshToken.java
@@ -0,0 +1,29 @@
+package com.inglo.giggle.security.domain.redis;
+
+import jakarta.persistence.Column;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash(value = "refresh_token", timeToLive = 60 * 60 * 24 * 14) // 14일
+public class RefreshToken {
+    @Id
+    @Column(name = "account_id")
+    private UUID accountId;
+
+    @Column(name = "value")
+    private String value;
+
+    @Builder
+    public RefreshToken(UUID accountId, String value) {
+        this.accountId = accountId;
+        this.value = value;
+    }
+}
