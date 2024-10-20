1b684136098192ffdc323339cfc26f3be1e65537
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-10-20 15:27:33 +0900

    [API-1.1] IGS-15 feat: 사용자 로그인 구현

---
+++ src/main/java/com/inglo/giggle/security/usecase/LoginByDefaultUseCase.java
@@ -0,0 +1,16 @@
+package com.inglo.giggle.security.usecase;
+
+import com.inglo.giggle.core.annotation.bean.UseCase;
+import com.inglo.giggle.security.dto.response.DefaultJsonWebTokenDto;
+import com.inglo.giggle.security.info.CustomUserPrincipal;
+
+@UseCase
+public interface LoginByDefaultUseCase {
+
+    /**
+     * Security에서 사용되는 Login 유스케이스
+     * @param principal UserPrincipal
+     * @param jsonWebTokenDto DefaultJsonWebTokenDto
+     */
+    void execute(CustomUserPrincipal principal, DefaultJsonWebTokenDto jsonWebTokenDto);
+}
