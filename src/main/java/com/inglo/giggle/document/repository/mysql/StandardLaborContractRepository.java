e466fcebbe5545206a273b662e3ea19121881d3f
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-10-22 18:15:38 +0900

    IGS-17 feat: StandardLaborContract 도메인 구현

---
+++ src/main/java/com/inglo/giggle/document/repository/mysql/StandardLaborContractRepository.java
@@ -0,0 +1,7 @@
+package com.inglo.giggle.document.repository.mysql;
+
+import com.inglo.giggle.document.domain.StandardLaborContract;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StandardLaborContractRepository extends JpaRepository<StandardLaborContract, Long>{
+}
