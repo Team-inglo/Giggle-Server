ecb73109d0da3eb56d333e0501f9d4fa9e2d4243
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-10-22 18:12:03 +0900

    IGS-17 fix: 잘못된 ID value 수정

--- src/main/java/com/inglo/giggle/account/repository/mysql/OwnerRepository.java
+++ src/main/java/com/inglo/giggle/account/repository/mysql/OwnerRepository.java
@@ -3,5 +3,7 @@ package com.inglo.giggle.account.repository.mysql;
 import com.inglo.giggle.account.domain.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OwnerRepository extends JpaRepository<Owner, Long>{
+import java.util.UUID;
+
+public interface OwnerRepository extends JpaRepository<Owner, UUID>{
 }
