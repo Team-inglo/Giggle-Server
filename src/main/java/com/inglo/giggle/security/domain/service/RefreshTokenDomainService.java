05a209631c008b7960c712d379524f85704089ae
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-10-23 07:09:27 +0900

    IGS-19 feat: RefreshTokenDomainService 구현

---
+++ src/main/java/com/inglo/giggle/security/domain/service/RefreshTokenDomainService.java
@@ -0,0 +1,16 @@
+package com.inglo.giggle.security.domain.service;
+
+import com.inglo.giggle.security.domain.redis.RefreshToken;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class RefreshTokenDomainService {
+    public RefreshToken createRefreshToken(UUID accountId, String refreshToken) {
+        return RefreshToken.builder()
+                .accountId(accountId)
+                .value(refreshToken)
+                .build();
+    }
+}
