cb6a81e5fdea8f58026780072166ce93f69f3998
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-10-23 07:00:55 +0900

    IGS-19 feat: OwnerDomainService 구현

---
+++ src/main/java/com/inglo/giggle/account/domain/service/OwnerDomainService.java
@@ -0,0 +1,34 @@
+package com.inglo.giggle.account.domain.service;
+
+import com.inglo.giggle.account.domain.Owner;
+import com.inglo.giggle.core.domain.Address;
+import com.inglo.giggle.security.application.dto.request.SignUpDefaultOwnerRequestDto;
+import com.inglo.giggle.security.domain.redis.TemporaryAccount;
+import com.inglo.giggle.security.domain.type.ESecurityProvider;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OwnerDomainService {
+    public Owner createOwner(ESecurityProvider securityProvider,
+                             TemporaryAccount temporaryAccount,
+                             String password,
+                             String iconImgUrl,
+                             SignUpDefaultOwnerRequestDto signUpDefaultOwnerRequestDto,
+                             Address address)
+    {
+        return Owner.builder()
+                .provider(securityProvider)
+                .serialId(temporaryAccount.getId())
+                .password(password)
+                .email(temporaryAccount.getEmail())
+                .profileImgUrl(iconImgUrl)
+                .phoneNumber(signUpDefaultOwnerRequestDto.signUpDefaultOwnerOwnerInfo().phoneNumber())
+                .companyName(signUpDefaultOwnerRequestDto.signUpDefaultOwnerOwnerInfo().companyName())
+                .ownerName(signUpDefaultOwnerRequestDto.signUpDefaultOwnerOwnerInfo().ownerName())
+                .companyRegistrationNumber(signUpDefaultOwnerRequestDto.signUpDefaultOwnerOwnerInfo().companyRegistrationNumber())
+                .marketingAllowed(signUpDefaultOwnerRequestDto.marketingAllowed())
+                .notificationAllowed(signUpDefaultOwnerRequestDto.notificationAllowed())
+                .address(address)
+                .build();
+    }
+}
