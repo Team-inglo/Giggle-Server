48ac88e2cb7f3cc6f538df7b4de464863bf5a9ba
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-10-23 07:06:46 +0900

    IGS-19 feat: AccountDomainService 구현

---
+++ src/main/java/com/inglo/giggle/security/domain/service/AccountDomainService.java
@@ -0,0 +1,29 @@
+package com.inglo.giggle.security.domain.service;
+
+import com.inglo.giggle.security.application.dto.request.SignUpDefaultTemporaryRequestDto;
+import com.inglo.giggle.security.domain.mysql.Account;
+import com.inglo.giggle.security.domain.redis.TemporaryAccount;
+import com.inglo.giggle.security.domain.type.ESecurityRole;
+import com.inglo.giggle.security.info.CustomUserPrincipal;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountDomainService {
+
+    public TemporaryAccount createTemporaryAccount(SignUpDefaultTemporaryRequestDto requestDto) {
+        return TemporaryAccount.builder()
+                .id(requestDto.id())
+                .email(requestDto.email())
+                .password(requestDto.password())
+                .accountType(ESecurityRole.fromString(requestDto.accountType()))
+                .build();
+    }
+
+    public void changePassword(Account account, String newPassword) {
+        account.updatePassword(newPassword);
+    }
+
+    public CustomUserPrincipal createCustomUserPrincipalByAccount(Account account) {
+        return CustomUserPrincipal.create(account);
+    }
+}
