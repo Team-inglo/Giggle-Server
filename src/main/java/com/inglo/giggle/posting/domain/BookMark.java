075fc0ed8fd39eb2443e85c14e4d0e7a591ecb0c
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-10-22 18:15:54 +0900

    IGS-17 feat: BookMark 도메인 구현

---
+++ src/main/java/com/inglo/giggle/posting/domain/BookMark.java
@@ -0,0 +1,41 @@
+package com.inglo.giggle.posting.domain;
+
+import com.inglo.giggle.account.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "book_marks")
+public class BookMark {
+    /* -------------------------------------------- */
+    /* Default Column ----------------------------- */
+    /* -------------------------------------------- */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /* -------------------------------------------- */
+    /* Many To One Mapping ------------------------ */
+    /* -------------------------------------------- */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "job_posting_id", nullable = false)
+    private JobPosting jobPosting;
+
+    /* -------------------------------------------- */
+    /* Methods ------------------------------------ */
+    /* -------------------------------------------- */
+    @Builder
+    public BookMark(User user, JobPosting jobPosting) {
+        this.user = user;
+        this.jobPosting = jobPosting;
+    }
+}
