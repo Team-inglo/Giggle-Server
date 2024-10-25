d2215e1f8c3b32770b5f2c0ffa5a270a954228fe
jjuuuunnii <jake991110@naver.com>
2024-10-25 19:29:09 +0900

    IGS-21: feature: 6.1 API 컨트롤러 반환 타입 추가

--- src/main/java/com/inglo/giggle/posting/application/controller/query/PostingQueryV1Controller.java
+++ src/main/java/com/inglo/giggle/posting/application/controller/query/PostingQueryV1Controller.java
@@ -2,12 +2,11 @@ package com.inglo.giggle.posting.application.controller.query;
 
 import com.inglo.giggle.core.annotation.security.AccountID;
 import com.inglo.giggle.core.dto.ResponseDto;
+import com.inglo.giggle.posting.application.dto.response.ReadUserAppliedJobListResponseDto;
+import com.inglo.giggle.posting.application.usecase.ReadUserAppliedJobDetailUseCase;
 import com.inglo.giggle.posting.application.usecase.ReadUserAppliedJobListUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -17,12 +16,13 @@ import java.util.UUID;
 public class PostingQueryV1Controller {
 
     private final ReadUserAppliedJobListUseCase readUserAppliedJobListUseCase;
+    private final ReadUserAppliedJobDetailUseCase readUserAppliedJobDetailUseCase;
 
     /**
      * 6.1 (유학생) 지원한 공고 리스트 조회하기
      */
     @GetMapping("/users/user-owner-job-postings/overviews")
-    public ResponseDto<?> readUserAppliedJobList(
+    public ResponseDto<ReadUserAppliedJobListResponseDto> readUserAppliedJobList(
             @AccountID UUID accountId,
             @RequestParam(name = "page", defaultValue = "1") Integer page,
             @RequestParam(name = "size", defaultValue = "10") Integer size,
@@ -35,6 +35,20 @@ public class PostingQueryV1Controller {
                 size,
                 sort,
                 status
+        ));
+    }
+
+    /**
+     * 6.2 (유학생) 지원한 공고 상태 상세 조회하기
+     */
+    @GetMapping("/users/user-owner-job-postings/{user-owner-job-postings-id}/details")
+    public ResponseDto<?> readUserAppliedJobDetail(
+            @AccountID UUID accountId,
+            @PathVariable(name = "user-owner-job-postings-id") Long userOwnerJobPostingsId
+    ) {
+        return ResponseDto.ok(readUserAppliedJobDetailUseCase.execute(
+                accountId,
+                userOwnerJobPostingsId
         ));
     }
 }
