6cfcd404acdd4f2a620f6898a8a9231f76bdf19f
jjuuuunnii <jake991110@naver.com>
2024-10-26 03:12:53 +0900

    IGS-21 refactor: 6.2 (유학생) 지원한 공고 상태 상세 조회하기 수정

--- src/main/java/com/inglo/giggle/posting/application/service/ReadUserOwnerJobPostingDetailService.java
+++ src/main/java/com/inglo/giggle/posting/application/service/ReadUserOwnerJobPostingDetailService.java
@@ -5,6 +5,7 @@ import com.inglo.giggle.core.exception.type.CommonException;
 import com.inglo.giggle.posting.application.dto.response.ReadUserOwnerJobPostingDetailResponseDto;
 import com.inglo.giggle.posting.application.usecase.ReadUserOwnerJobPostingDetailUseCase;
 import com.inglo.giggle.posting.domain.UserOwnerJobPosting;
+import com.inglo.giggle.posting.domain.service.UserOwnerJobPostingService;
 import com.inglo.giggle.posting.repository.mysql.UserOwnerJobPostingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ import java.util.UUID;
 public class ReadUserOwnerJobPostingDetailService implements ReadUserOwnerJobPostingDetailUseCase {
 
     private final UserOwnerJobPostingRepository userOwnerJobPostingRepository;
+    private final UserOwnerJobPostingService userOwnerJobPostingService;
 
     @Override
     public ReadUserOwnerJobPostingDetailResponseDto execute(UUID accountId, Long userOwnerJobPostingsId) {
@@ -25,11 +27,9 @@ public class ReadUserOwnerJobPostingDetailService implements ReadUserOwnerJobPos
                 .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_RESOURCE));
 
         // 조회한 UserOwnerJobPosting이 accountId와 일치하는지 확인
-        if (!userOwnerJobPosting.getUser().getId().equals(accountId)) {
-            throw new CommonException(ErrorCode.NOT_FOUND_RESOURCE);
-        }
+        userOwnerJobPostingService.validateUserOwnerJobPostingAndUser(userOwnerJobPosting, accountId);
 
-        // Dto 변환
+        // DTO 변환
         return ReadUserOwnerJobPostingDetailResponseDto.fromEntity(
                 userOwnerJobPosting
         );
--- src/main/java/com/inglo/giggle/posting/domain/service/UserOwnerJobPostingService.java
+++ src/main/java/com/inglo/giggle/posting/domain/service/UserOwnerJobPostingService.java
@@ -1,10 +1,13 @@
 package com.inglo.giggle.posting.domain.service;
 
+import com.inglo.giggle.core.exception.error.ErrorCode;
+import com.inglo.giggle.core.exception.type.CommonException;
 import com.inglo.giggle.posting.domain.UserOwnerJobPosting;
 import com.inglo.giggle.posting.domain.type.EApplicationStep;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserOwnerJobPostingService {
@@ -13,5 +16,11 @@ public class UserOwnerJobPostingService {
         return Math.toIntExact(userOwnerJobPostingList.stream()
                 .filter(userOwnerJobPosting -> userOwnerJobPosting.getStep().equals(EApplicationStep.APPLICATION_SUCCESS))
                 .count());
+    }
+
+    public void validateUserOwnerJobPostingAndUser(UserOwnerJobPosting userOwnerJobPosting, UUID accountId) {
+        if (!userOwnerJobPosting.getUser().getId().equals(accountId)) {
+            throw new CommonException(ErrorCode.NOT_FOUND_RESOURCE);
+        }
     }
 }
