d5e5bc23323dbce12a47ef3334b4645af808863b
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-10-26 02:36:21 +0900

    IGS-22 add: EnableRedisRepositories Annotation

--- src/main/java/com/inglo/giggle/GiggleSeverApplication.java
+++ src/main/java/com/inglo/giggle/GiggleSeverApplication.java
@@ -3,6 +3,7 @@ package com.inglo.giggle;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,6 +11,7 @@ import java.util.TimeZone;
 
 @EnableAsync
 @EnableJpaRepositories(basePackages = "com.inglo.giggle.*.repository.mysql")
+@EnableRedisRepositories(basePackages = "com.inglo.giggle.*.repository.redis")
 @SpringBootApplication
 public class GiggleSeverApplication {
 
