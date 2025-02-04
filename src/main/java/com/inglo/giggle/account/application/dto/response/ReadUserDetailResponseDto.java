4e8b64af269716527e86bf629b8e0b17237ba321
장동겸 <jangelliot0404@dgu.ac.kr>
2025-02-05 00:05:49 +0900

    ✨ Feature - 3.1 유저 정보 조회에 주소 추가 (#123)

--- src/main/java/com/inglo/giggle/account/application/dto/response/ReadUserDetailResponseDto.java
+++ src/main/java/com/inglo/giggle/account/application/dto/response/ReadUserDetailResponseDto.java
@@ -2,6 +2,7 @@ package com.inglo.giggle.account.application.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.inglo.giggle.account.domain.User;
+import com.inglo.giggle.address.domain.Address;
 import com.inglo.giggle.core.dto.SelfValidating;
 import com.inglo.giggle.core.utility.DateTimeUtil;
 import jakarta.validation.constraints.NotBlank;
@@ -44,6 +45,9 @@ public class ReadUserDetailResponseDto extends SelfValidating<ReadUserDetailResp
     @JsonProperty("phone_number")
     private final String phoneNumber;
 
+    @JsonProperty("address")
+    private final AddressDto address;
+
     @Builder
     public ReadUserDetailResponseDto(
             String profileImgUrl,
@@ -53,7 +57,8 @@ public class ReadUserDetailResponseDto extends SelfValidating<ReadUserDetailResp
             String gender,
             String nationality,
             String visa,
-            String phoneNumber
+            String phoneNumber,
+            AddressDto address
     ) {
         this.profileImgUrl = profileImgUrl;
         this.firstName = firstName;
@@ -63,8 +68,79 @@ public class ReadUserDetailResponseDto extends SelfValidating<ReadUserDetailResp
         this.nationality = nationality;
         this.visa = visa;
         this.phoneNumber = phoneNumber;
+        this.address = address;
 
         this.validateSelf();
+    }
+
+    @Getter
+    public static class AddressDto {
+
+        @NotBlank(message = "address_name은 null일 수 없습니다.")
+        @JsonProperty("address_name")
+        private final String addressName;
+
+        @NotBlank(message = "region_1depth_name은 null일 수 없습니다.")
+        @JsonProperty("region_1depth_name")
+        private final String region1depthName;
+
+        @NotBlank(message = "region_2depth_name은 null일 수 없습니다.")
+        @JsonProperty("region_2depth_name")
+        private final String region2depthName;
+
+        @NotBlank(message = "region_3depth_name은 null일 수 없습니다.")
+        @JsonProperty("region_3depth_name")
+        private final String region3depthName;
+
+        @NotBlank(message = "region_4depth_name은 null일 수 없습니다.")
+        @JsonProperty("region_4depth_name")
+        private final String region4depthName;
+
+        @NotBlank(message = "address_detail은 null일 수 없습니다.")
+        @JsonProperty("address_detail")
+        private final String addressDetail;
+
+        @NotNull(message = "longitude는 null일 수 없습니다.")
+        @JsonProperty("longitude")
+        private final Double longitude;
+
+        @NotNull(message = "latitude는 null일 수 없습니다.")
+        @JsonProperty("latitude")
+        private final Double latitude;
+
+        @Builder
+        public AddressDto(
+                String addressName,
+                String region1depthName,
+                String region2depthName,
+                String region3depthName,
+                String region4depthName,
+                String addressDetail,
+                Double longitude,
+                Double latitude
+        ) {
+            this.addressName = addressName;
+            this.region1depthName = region1depthName;
+            this.region2depthName = region2depthName;
+            this.region3depthName = region3depthName;
+            this.region4depthName = region4depthName;
+            this.addressDetail = addressDetail;
+            this.longitude = longitude;
+            this.latitude = latitude;
+        }
+
+        public static ReadUserDetailResponseDto.AddressDto fromEntity(Address address) {
+            return ReadUserDetailResponseDto.AddressDto.builder()
+                    .addressName(address.getAddressName())
+                    .region1depthName(address.getRegion1DepthName())
+                    .region2depthName(address.getRegion2DepthName())
+                    .region3depthName(address.getRegion3DepthName())
+                    .region4depthName(address.getRegion4DepthName())
+                    .addressDetail(address.getAddressDetail())
+                    .longitude(address.getLongitude())
+                    .latitude(address.getLatitude())
+                    .build();
+        }
     }
 
     public static ReadUserDetailResponseDto fromEntity(User user) {
@@ -77,6 +153,7 @@ public class ReadUserDetailResponseDto extends SelfValidating<ReadUserDetailResp
                 .nationality(user.getNationality())
                 .visa(user.getVisa().name())
                 .phoneNumber(user.getPhoneNumber())
+                .address(AddressDto.fromEntity(user.getAddress()))
                 .build();
     }
 }
