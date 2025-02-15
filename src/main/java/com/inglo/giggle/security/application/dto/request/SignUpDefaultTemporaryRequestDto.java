598cfd8a8bc3a16a407d4ea3114d452460d96c4f
장동겸 <jangelliot0404@dgu.ac.kr>
2025-02-15 23:51:33 +0900

    🔨 Fix - 아이디와 이메일 통합 (#151)

--- src/main/java/com/inglo/giggle/security/application/dto/request/SignUpDefaultTemporaryRequestDto.java
+++ src/main/java/com/inglo/giggle/security/application/dto/request/SignUpDefaultTemporaryRequestDto.java
@@ -6,9 +6,11 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record SignUpDefaultTemporaryRequestDto(
-        @JsonProperty("id")
-        @NotBlank(message = "아이디를 입력해주세요.")
-        @Pattern(regexp = "^[A-Za-z0-9]{1,320}$", message = "아이디는 1글자 이상 320자 이하의 영어 대소문자, 숫자로 입력해주세요.")
+        @JsonProperty("email")
+        @NotBlank(message = "이메일을 입력해주세요.")
+        @Pattern(
+                regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$",
+                message = "아이디 형식이 올바르지 않습니다.")
         String id,
 
         @JsonProperty("password")
