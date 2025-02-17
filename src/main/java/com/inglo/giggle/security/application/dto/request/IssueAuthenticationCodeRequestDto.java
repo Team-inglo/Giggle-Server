c70bc648cd81576cfbb532a2c63f7c2fa037bae1
jjuuuunnii <jake991110@naver.com>
2025-02-17 18:39:27 +0900

    feat: IssueAuthenticationCodeRequestDto.java Pattern 추가

--- src/main/java/com/inglo/giggle/security/application/dto/request/IssueAuthenticationCodeRequestDto.java
+++ src/main/java/com/inglo/giggle/security/application/dto/request/IssueAuthenticationCodeRequestDto.java
@@ -3,10 +3,14 @@ package com.inglo.giggle.security.application.dto.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record IssueAuthenticationCodeRequestDto(
         @JsonProperty("email")
         @NotBlank(message = "이메일 정보가 존재하지 않습니다.")
+        @Pattern(
+                regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$",
+                message = "이메일 형식이 올바르지 않습니다.")
         String email
 ) {
 }
