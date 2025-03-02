554b60f94cf06ffbe81b396537842b3edbe61400
jjuuuunnii <jake991110@naver.com>
2025-03-02 18:46:55 +0900

    Refactor: Security 권한 분리

--- src/main/java/com/inglo/giggle/core/interceptor/pre/HttpAccountIDInterceptor.java
+++ src/main/java/com/inglo/giggle/core/interceptor/pre/HttpAccountIDInterceptor.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.util.regex.Pattern;
+
 @Component
 public class HttpAccountIDInterceptor implements HandlerInterceptor {
 
@@ -17,6 +19,16 @@ public class HttpAccountIDInterceptor implements HandlerInterceptor {
             HttpServletResponse response,
             Object handler
     ) throws Exception {
+
+        String requestURI = request.getRequestURI();
+
+        // 정규식 기반 예외 처리
+        for (String pattern : Constants.REGEX_NO_NEED_AUTH_URLS) {
+            if (Pattern.matches(pattern, requestURI)) {
+                return HandlerInterceptor.super.preHandle(request, response, handler);
+            }
+        }
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         request.setAttribute(Constants.ACCOUNT_ID_ATTRIBUTE_NAME, authentication.getName());
