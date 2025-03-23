b141bd979fb5e548f78d1be722c9c152e1660385
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-03-24 02:23:51 +0900

    feat/#233: AdminNotification Entity 구현

---
+++ src/main/java/com/inglo/giggle/notification/domain/AdminNotification.java
@@ -0,0 +1,60 @@
+package com.inglo.giggle.notification.domain;
+
+import com.inglo.giggle.core.dto.BaseEntity;
+import com.inglo.giggle.security.domain.type.ESecurityRole;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "admin_notifications")
+@SQLDelete(sql = "UPDATE admin_notifications SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
+public class AdminNotification extends BaseEntity {
+
+    /* -------------------------------------------- */
+    /* Default Column ----------------------------- */
+    /* -------------------------------------------- */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /* -------------------------------------------- */
+    /* Information Column ------------------------- */
+    /* -------------------------------------------- */
+    @Column(name = "title", length = 50, nullable = false)
+    private String title;
+
+    @Column(name = "message", length = 200, nullable = false)
+    private String message;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private ESecurityRole role;
+
+    @Column(name = "is_advertisement", nullable = false)
+    private Boolean isAdvertisement;
+
+    /* -------------------------------------------- */
+    /* Method ------------------------------------ */
+    /* -------------------------------------------- */
+
+    @Builder
+    public AdminNotification(
+            String title,
+            String message,
+            ESecurityRole role,
+            Boolean isAdvertisement
+    ) {
+        this.title = title;
+        this.message = message;
+        this.role = role;
+        this.isAdvertisement = isAdvertisement;
+    }
+}
