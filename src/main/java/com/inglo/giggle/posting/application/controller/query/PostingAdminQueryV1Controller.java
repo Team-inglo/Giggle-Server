eab0a3c00b922f8641f5d08b3559e43a5e16d2dc
jjuuuunnii <jake991110@naver.com>
2025-03-23 18:47:34 +0900

    feat: PostingAdminQueryV1Controller 구현

---
+++ src/main/java/com/inglo/giggle/posting/application/controller/query/PostingAdminQueryV1Controller.java
@@ -0,0 +1,11 @@
+package com.inglo.giggle.posting.application.controller.query;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PostingAdminQueryV1Controller {
+
+}
+
