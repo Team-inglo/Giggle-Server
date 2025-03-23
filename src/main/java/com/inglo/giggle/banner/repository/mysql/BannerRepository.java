2359d797efca0c3bc6636c3d3d577612edf118c7
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-03-24 02:25:07 +0900

    fix/#233: GUEST 배너 기본 조회 추가

--- src/main/java/com/inglo/giggle/banner/repository/mysql/BannerRepository.java
+++ src/main/java/com/inglo/giggle/banner/repository/mysql/BannerRepository.java
@@ -2,12 +2,16 @@ package com.inglo.giggle.banner.repository.mysql;
 
 import com.inglo.giggle.banner.domain.Banner;
 import com.inglo.giggle.security.domain.type.ESecurityRole;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface BannerRepository extends JpaRepository<Banner, Long>{
-    List<Banner> findByRole(ESecurityRole role);
+
+    @Query("SELECT b FROM Banner b WHERE b.role = :role OR b.role = 'GUEST'")
+    List<Banner> findByRole(@Param("role") ESecurityRole role);
 }
