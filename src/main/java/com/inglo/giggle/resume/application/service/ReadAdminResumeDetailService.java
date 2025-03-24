f22caad27ebf378c6d7f6391b4a5b0d7b8da75f5
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2025-03-25 01:35:07 +0900

    feat/#243: 7.20 (어드민) 이력서 조회하기 API 구현

---
+++ src/main/java/com/inglo/giggle/resume/application/service/ReadAdminResumeDetailService.java
@@ -0,0 +1,50 @@
+package com.inglo.giggle.resume.application.service;
+
+import com.inglo.giggle.core.exception.error.ErrorCode;
+import com.inglo.giggle.core.exception.type.CommonException;
+import com.inglo.giggle.resume.application.dto.response.ReadAdminResumeDetailResponseDto;
+import com.inglo.giggle.resume.application.usecase.ReadAdminResumeDetailUseCase;
+import com.inglo.giggle.resume.domain.Education;
+import com.inglo.giggle.resume.domain.LanguageSkill;
+import com.inglo.giggle.resume.domain.Resume;
+import com.inglo.giggle.resume.domain.WorkExperience;
+import com.inglo.giggle.resume.repository.mysql.EducationRepository;
+import com.inglo.giggle.resume.repository.mysql.LanguageSkillRepository;
+import com.inglo.giggle.resume.repository.mysql.ResumeRepository;
+import com.inglo.giggle.resume.repository.mysql.WorkExperienceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ReadAdminResumeDetailService implements ReadAdminResumeDetailUseCase {
+
+    private final ResumeRepository resumeRepository;
+    private final EducationRepository educationRepository;
+    private final LanguageSkillRepository languageSkillRepository;
+    private final WorkExperienceRepository workExperienceRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public ReadAdminResumeDetailResponseDto execute(UUID resumeId) {
+        // Resume 조회
+        Resume resume = resumeRepository.findById(resumeId)
+                .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_RESOURCE));
+
+        // education 조회
+        List<Education> educations = educationRepository.findAllByResume(resume);
+
+        // LanguageSkill 조회
+        LanguageSkill languageSkill = languageSkillRepository.findByResume(resume)
+                .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_RESOURCE));
+
+        // WorkExperience 조회
+        List<WorkExperience> workExperiences = workExperienceRepository.findAllByResume(resume);
+
+        return ReadAdminResumeDetailResponseDto.of(resume, workExperiences, educations, languageSkill, resume.getUser());
+    }
+}
