adb91dc3a1b9b06b8f241c65ab98b0527d23c164
jjuuuunnii <jake991110@naver.com>
2025-03-30 03:19:44 +0900

    fix: DeviceToken 생성 및 업데이트 Dto, 로직 수정

--- src/main/java/com/inglo/giggle/security/application/dto/request/UpdateDeviceTokenRequestDto.java
+++ src/main/java/com/inglo/giggle/security/application/dto/request/UpdateDeviceTokenRequestDto.java
@@ -7,6 +7,10 @@ public record UpdateDeviceTokenRequestDto(
 
         @JsonProperty("device_token")
         @NotBlank(message = "deviceToken은 null일 수 없습니다.")
-        String deviceToken
+        String deviceToken,
+
+        @JsonProperty("device_id")
+        @NotBlank(message = "deviceId는 null일 수 없습니다.")
+        String deviceId
 ) {
 }
