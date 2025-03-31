2edbebcced7950024e42a650db908cfaa4de2b24
jjuuuunnii <jake991110@naver.com>
2025-04-01 01:50:47 +0900

    feat: 0.1 최신 버전 조회하기 API 구현

---
+++ src/main/java/com/inglo/giggle/version/application/usecase/ReadRecentVersionUseCase.java
@@ -0,0 +1,16 @@
+package com.inglo.giggle.version.application.usecase;
+
+import com.inglo.giggle.core.annotation.bean.UseCase;
+import com.inglo.giggle.version.application.dto.response.ReadRecentVersionResponseDto;
+import com.inglo.giggle.version.domain.type.EOsType;
+
+@UseCase
+public interface ReadRecentVersionUseCase {
+
+    /**
+     * 0.1 최근 버전 조회
+     */
+    ReadRecentVersionResponseDto execute(
+            EOsType osType
+    );
+}
